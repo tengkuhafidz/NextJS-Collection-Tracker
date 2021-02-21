21c0e38abee98f230974fec124aec675efd52115
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-02-21 23:30:31 +0800

    add capture nric feature

---
+++ cypress/e2e/capture-collection.js
@@ -0,0 +1,30 @@
+describe('Capture Collection', () => {
+	it('should disable the next button when user does not provide any nric', () => {
+		// GIVEN user is logged in
+		cy.login('hafidz', 'Hafidz123!')
+		// WHEN user does not provide NRIC
+		cy.findByPlaceholderText('NRIC').should('be.visible')
+		// THEN next button should be disabled
+		cy.findByRole('button', {name: 'Next'}).should('be.disabled')
+	})
+
+	it('should display invalid nric error message when user provides invalid beneficiary nric', () => {
+		// GIVEN user is logged in
+		cy.login('hafidz', 'Hafidz123!')
+		// WHEN user provides invalid beneficiary nric
+		cy.findByPlaceholderText('NRIC').type('S7654321D')
+		cy.findByRole('button', {name: 'Next'}).click()
+		// THEN user should see invalid nric error message
+		cy.findByText('Invalid NRIC')
+	})
+
+	// it('should identify the beneficiary given valid nric', () => {
+	// 	// GIVEN user is logged in
+	// 	cy.login('hafidz', 'Hafidz123!')
+	// 	// WHEN user provides valid beneficiary nric
+	// 	cy.findByPlaceholderText('NRIC').type('S1111111D')
+	// 	cy.findByRole('button', {name: 'Next'}).click()
+	// 	// THEN user should be redirected to collection tracker form
+	// 	// AND able to identify the beneficiary
+	// })
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,66 @@
-import Head from 'next/head'
+import Router from 'next/router'
+import {parseCookies} from 'nookies'
+import React, {useState} from 'react'
+import {ErrorBox} from '../components/error-box'
+import Layout from '../components/layout'
+import * as StrapiService from '../services/strapi-services'
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title>NextJS Boilerplate</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+	const [nric, setNric] = useState(null)
+	const [isValidNric, setIsValidNric] = useState(true)
+	const {username} = parseCookies()
 
-			<main>
-				<h1>Hello World</h1>
-			</main>
-		</div>
+	const handleNricChange = (nric: string) => {
+		setNric(nric)
+		setIsValidNric(true)
+	}
+
+	const handleCaptureNric = async () => {
+		const beneficiary = await StrapiService.getBeneficiary(nric)
+
+		if (!beneficiary) {
+			setIsValidNric(false)
+		} else {
+			Router.push('/form')
+		}
+	}
+
+	const renderInvalidNricError = () => {
+		if (!isValidNric && !!nric) {
+			return <ErrorBox message="Invalid NRIC" />
+		}
+
+		return null
+	}
+
+	return (
+		<Layout>
+			<div>
+				<div className="flex">
+					<p className="p-2 text-gray-500 inline flex-grow">{username}</p>
+					<p className="p-2 text-gray-500 inline">logout</p>
+				</div>
+
+				<main className="p-5">
+					<h1 className="text-3xl font-bold text-center mt-32 mb-8">
+						Capture NRIC
+					</h1>
+					<input
+						type="text"
+						placeholder="NRIC"
+						className="standard-input"
+						onChange={e => handleNricChange(e.target.value)}
+					/>
+					{renderInvalidNricError()}
+					<button
+						className="text-xl bg-black rounded-md text-white w-full p-3 mt-8 disabled:opacity-60 disabled:cursor-not-allowed"
+						disabled={!nric}
+						onClick={handleCaptureNric}
+					>
+						Next
+					</button>
+				</main>
+			</div>
+		</Layout>
 	)
 }
