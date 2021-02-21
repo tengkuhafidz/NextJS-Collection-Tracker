78074bf0ceeaca47ee7acc708f3a351be689c504
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-02-21 16:35:05 +0800

    enhance login UI

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,11 +8,23 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main>
-				<h1>Collection Tracker</h1>
-				<input type="text" placeholder="Email / Username" />
-				<input type="password" placeholder="Password" />
-				<button>Login</button>
+			<main className="p-5">
+				<h1 className="text-3xl font-bold text-center mt-32 mb-16">
+					Collection Tracker
+				</h1>
+				<input
+					type="text"
+					placeholder="Email / Username"
+					className="standard-input"
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					className="standard-input"
+				/>
+				<button className="text-xl bg-black rounded-md text-white w-full p-3 mt-8">
+					Login
+				</button>
 			</main>
 		</div>
 	)
