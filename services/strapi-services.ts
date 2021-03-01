17cf56ab8f2748ae75c5822ec4de6ff1f221ab93
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-03-01 20:56:44 +0800

    fix date format

--- services/strapi-services.ts
+++ services/strapi-services.ts
@@ -85,7 +85,7 @@ export const dateToday = () => {
 	return (
 		dt.getFullYear() +
 		'-' +
-		('0' + dt.getMonth() + 1).slice(-2) +
+		('0' + (dt.getMonth() + 1)).slice(-2) +
 		'-' +
 		('0' + dt.getDate()).slice(-2)
 	)
