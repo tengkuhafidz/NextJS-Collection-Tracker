83050ee34cc538ed0cdf71d2f6852ac1c20a617d
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-03-03 00:45:58 +0800

    add qr scanner

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,13 @@ import React, {useState} from 'react'
 import {ErrorBox} from '../components/error-box'
 import Layout from '../components/layout'
 import * as StrapiService from '../services/strapi-services'
+import QrReader from 'react-qr-scanner'
 
 export default function Home() {
 	const [customerId, setCustomerId] = useState(null)
 	const [isValidCustomerId, setIsValidCustomerId] = useState(true)
 	const [isLoading, setIsLoading] = useState(false)
+	const [isScannerActive, setIsScannerActive] = useState(false)
 	const {username} = parseCookies()
 
 	const handleLogout = () => {
@@ -48,6 +50,20 @@ export default function Home() {
 		return null
 	}
 
+	const renderQrScanner = () => {
+		if (!isScannerActive) {
+			return null
+		}
+		return (
+			<QrReader
+				delay={300}
+				onScan={data => setCustomerId(data)}
+				onError={e => console.log(e)}
+				facingMode="rear"
+			/>
+		)
+	}
+
 	return (
 		<Layout>
 			<div>
@@ -60,14 +76,24 @@ export default function Home() {
 
 				<main className="p-5">
 					<h1 className="text-3xl font-bold text-center mt-32 mb-8">
-						Capture ID
+						Capture ID {customerId}
 					</h1>
-					<input
-						type="text"
-						placeholder="ID"
-						className="standard-input"
-						onChange={e => handleCustomerIdChange(e.target.value)}
-					/>
+					{renderQrScanner()}
+					<div className="flex">
+						<input
+							type="text"
+							placeholder="ID"
+							className="standard-input flex-grow m-0"
+							value={customerId}
+							onChange={e => handleCustomerIdChange(e.target.value)}
+						/>
+						<button
+							className="ml-1 px-2 bg-gray-800 text-white rounded-md w-12"
+							onClick={() => setIsScannerActive(!isScannerActive)}
+						>
+							{isScannerActive ? 'x' : 'scan'}
+						</button>
+					</div>
 					{renderInvalidCustomerIdError()}
 					<button
 						className={`text-xl bg-black rounded-md text-white w-full p-3 mt-8 disabled:opacity-60 disabled:cursor-not-allowed ${
