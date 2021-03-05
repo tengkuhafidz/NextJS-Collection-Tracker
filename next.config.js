3a0f307209381da54e6933c1593e198f9942bb6c
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-03-05 23:29:34 +0800

    make it pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+	pwa: {
+		dest: 'public',
+	},
 	publicRuntimeConfig: {
 		StrapiBaseUrl: process.env.NEXT_PUBLIC_STRAPI_BASE_URL,
 		MixpanelToken: process.env.MIXPANEL_TOKEN,
 	},
-}
+	future: {webpack5: true},
+})
